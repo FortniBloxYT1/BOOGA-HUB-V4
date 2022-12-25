bed0f894381f48f0e9bed71b1571ac9003efcd36
FortniBloxYT1 <98220426+FortniBloxYT1@users.noreply.github.com>
2022-12-26 00:25:00 +0100

    Update BOOGA-HUB-V4.lua

--- BOOGA-HUB-V4.lua
+++ BOOGA-HUB-V4.lua
@@ -224,7 +224,7 @@ end)
 
 local Main, MainSection = BoogaHub:addPage("Main", 5012544693)
 local MainSection = Main:addSection("Main | Section 1")
-MainSection:addButton("Last Update : 25/12/2022",function() end)
+MainSection:addButton("Last Update : 26/12/2022",function() end)
 MainSection:addToggle("No Slow", nil, function(NS)
 	if NS then
 		NSRun = RS.RenderStepped:Connect(function()
