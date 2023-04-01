74afd9562d7da426f17ae0ce57608b50132981fe
FortniBloxYT1 <98220426+FortniBloxYT1@users.noreply.github.com>
2023-04-01 18:38:15 +0200

    Update BOOGA-HUB V5.lua

--- BOOGA-HUB V5.lua
+++ BOOGA-HUB V5.lua
@@ -302,8 +302,9 @@ local MainSection = Main:addSection("Main | Section 1")
 MainSection:addButton("Last Update : 31/03/2023",function() end)
 task.spawn(function()
 	MainSection:addToggle("No Slow", ActualSettings.SaveNoSlow, function(NS)
-				
-		if math.random(1,20) == 1 and ActualSettings.AprilFools == "nil" then
+		local m = math.random(1,20)
+		print(m)
+		if m == 1 and ActualSettings.AprilFools == "nil" then
 			Settings.AprilFools = "true"
 			ActualSettings.AprilFools = "true"
 			Save()
